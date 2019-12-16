5860b5540673cd13f9ccd897603cc15f07cbc82c
00day0 <therandomuser11@gmail.com>
2019-12-16 21:31:27 +0100

    SystemUI: FOD icon picker [1/2]
    Co-authored-by: Stallix <thestallix@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5129,6 +5129,12 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * FOD icon picker
+         * @hide
+         */
+        public static final String FOD_ICON = "fod_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
