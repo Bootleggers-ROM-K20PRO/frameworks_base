1c8cbf76e64f1da5d0364cd115c9b686ec95bc21
AnkitGourav <Ankitgourav035@gmail.com>
2020-02-08 10:11:53 +0530

    SystemUI: Ambient pulse animation [1/2]
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5205,6 +5205,24 @@ public final class Settings {
         private static final Validator AMBIENT_NOTIFICATION_LIGHT_ACCENT_RIGHT_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Left Ambient Edge Light Repeat Mode
+         * @hide
+         */
+        public static final String PULSE_AMBIENT_LIGHT_REPEAT_MODE_LEFT = "pulse_ambient_light_repeat_mode_left";
+
+        /** @hide */
+        private static final Validator PULSE_AMBIENT_LIGHT_REPEAT_MODE_LEFT_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
+        /**
+         * Right Ambient Edge Light Repeat Mode
+         * @hide
+         */
+        public static final String PULSE_AMBIENT_LIGHT_REPEAT_MODE_RIGHT = "pulse_ambient_light_repeat_mode_right";
+
+        /** @hide */
+        private static final Validator PULSE_AMBIENT_LIGHT_REPEAT_MODE_RIGHT_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
+        /**
          * Whether to repeat edge light pulsing for all doze events
          * @hide
          */
@@ -5609,6 +5627,8 @@ public final class Settings {
             PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL,
             AMBIENT_NOTIFICATION_LIGHT_ACCENT_LEFT,
             AMBIENT_NOTIFICATION_LIGHT_ACCENT_RIGHT,
+            PULSE_AMBIENT_LIGHT_REPEAT_MODE_LEFT,
+            PULSE_AMBIENT_LIGHT_REPEAT_MODE_RIGHT,
             PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL_REPEAT,
         };
 
@@ -5825,6 +5845,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL);
             PRIVATE_SETTINGS.add(AMBIENT_NOTIFICATION_LIGHT_ACCENT_LEFT);
             PRIVATE_SETTINGS.add(AMBIENT_NOTIFICATION_LIGHT_ACCENT_RIGHT);
+            PRIVATE_SETTINGS.add(PULSE_AMBIENT_LIGHT_REPEAT_MODE_LEFT);
+            PRIVATE_SETTINGS.add(PULSE_AMBIENT_LIGHT_REPEAT_MODE_RIGHT);
             PRIVATE_SETTINGS.add(PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL_REPEAT);
         }
 
@@ -5992,6 +6014,8 @@ public final class Settings {
             VALIDATORS.put(PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL, PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL_VALIDATOR);
             VALIDATORS.put(AMBIENT_NOTIFICATION_LIGHT_ACCENT_LEFT, AMBIENT_NOTIFICATION_LIGHT_ACCENT_LEFT_VALIDATOR);
             VALIDATORS.put(AMBIENT_NOTIFICATION_LIGHT_ACCENT_RIGHT, AMBIENT_NOTIFICATION_LIGHT_ACCENT_RIGHT_VALIDATOR);
+            VALIDATORS.put(PULSE_AMBIENT_LIGHT_REPEAT_MODE_LEFT, PULSE_AMBIENT_LIGHT_REPEAT_MODE_LEFT_VALIDATOR);
+            VALIDATORS.put(PULSE_AMBIENT_LIGHT_REPEAT_MODE_RIGHT, PULSE_AMBIENT_LIGHT_REPEAT_MODE_RIGHT_VALIDATOR);
             VALIDATORS.put(PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL_REPEAT, PULSE_AMBIENT_LIGHT_PULSE_FOR_ALL_REPEAT_VALIDATOR);
         }
 
