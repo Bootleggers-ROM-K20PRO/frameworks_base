dbe67d3e134edc1604913464d967546d38c54aa9
taran107 <hora.cooltaran@gmail.com>
2020-03-09 17:31:43 +0530

    Revert "Status bar clock customizations [1/2]"
    This reverts commit db60a5878c68c6d18eecd91d9d282ec8f7f40e74.
    
    Change-Id: Ie396704475e949a4d4ab6fe751f9140968c5f15a

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -269,7 +269,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
-        mClockView.setQsHeader();
         mDateView = findViewById(R.id.date);
         mSpace = findViewById(R.id.space);
 
@@ -475,7 +474,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if (mExpanded == expanded) return;
         mExpanded = expanded;
         mHeaderQsPanel.setExpanded(expanded);
-        mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateEverything();
     }
 
