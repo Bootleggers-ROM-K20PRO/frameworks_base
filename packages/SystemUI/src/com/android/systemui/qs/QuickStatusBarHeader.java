4bfe227d257708b5d2009727a6d6e3f1f0a227ce
taran107 <hora.cooltaran@gmail.com>
2020-03-09 17:31:09 +0530

    Revert "base: SystemUI: tuner: fix hiding of statusbar clock"
    This reverts commit 9e654366a8a42243daaaf89273ee2f4310d82f56.

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -270,7 +270,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
         mClockView.setQsHeader();
-        mClockView.setClockHideableByUser(false);
         mDateView = findViewById(R.id.date);
         mSpace = findViewById(R.id.space);
 
